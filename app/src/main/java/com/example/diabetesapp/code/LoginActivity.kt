64f62de7e4341d14d1ba61f1e9b325f2480c94c3
BinaryWiz4rd <278437@student.pwr.edu.pl>
2024-12-25 18:14:38 +0100

    finished register and login

--- app/src/main/java/com/example/diabetesapp/code/LoginActivity.kt
+++ app/src/main/java/com/example/diabetesapp/code/LoginActivity.kt
@@ -1,23 +1,79 @@
 package com.example.diabetesapp.code
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.diabetesapp.R
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var signupRedirectText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = FirebaseAuth.getInstance()
+        loginEmail = findViewById(R.id.login_email)
+        loginPassword = findViewById(R.id.login_password)
+        loginButton = findViewById(R.id.login_button)
+        signupRedirectText = findViewById(R.id.signupRedirectText)
+
+        loginButton.setOnClickListener {
+            val email = loginEmail.text.toString()
+            val pass = loginPassword.text.toString()
+
+            if (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (pass.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(email, pass)
+                        .addOnSuccessListener { authResult: AuthResult ->
+                            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }
+                        .addOnFailureListener { e: Exception ->
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                        }
+                } else {
+                    loginPassword.error = "Password cannot be empty"
+                }
+            } else if (email.isEmpty()) {
+                loginEmail.error = "Email cannot be empty"
+            } else {
+                loginEmail.error = "Please enter a valid email"
+            }
+        }
+
+        signupRedirectText.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
+
+
+
+//class LoginActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_login)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
